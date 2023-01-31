400039488b1f6c28876bbfc5956f9c575902ceb4
GusEngers <gustavoengers6441@gmail.com>
2023-01-31 10:50:14 -0300

    Tipos de datos
    - enum
    - interface
    - type

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,38 @@ console.log(`${textoDos}... añadiendo el tipo de dato!`);
 
 // textoDos = 2;
 // console.log(`${textoDos}... cambiandole a un tipo de dato erroneo!`);
+
+// Enum's:
+enum Ejemplo {
+	'completo',
+	'incompleto',
+}
+
+let ejemploEnum: Ejemplo = Ejemplo.completo;
+console.log('Ejemplo enum:', ejemploEnum);
+
+// Interfaces:
+interface DatoPersonal {
+	nombre: string;
+	edad: number;
+	nacionalidad: string;
+}
+
+let miDato: DatoPersonal = {
+	nombre: 'Gustavo',
+	edad: 22,
+	nacionalidad: 'argentino',
+};
+console.log('Estos son mis datos:', miDato);
+
+// Types:
+type Producto = {
+	nombre: string;
+	precio: number;
+};
+
+let miProducto: Producto = {
+	nombre: 'Arroz',
+	precio: 450,
+};
+console.log('Este es mi producto', miProducto);
