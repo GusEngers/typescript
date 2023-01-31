820085be55145fbf2e1ec12681a52a987c943282
GusEngers <gustavoengers6441@gmail.com>
2023-01-31 09:34:12 -0300

    variables

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-console.log('Hola mundo! Esto es typescript');
-console.log('Soy yo!');
+console.log('Hola mundo! Esto es Typescript');
+
+// Variables:
+
+// [TIPADO DÉBIL] → Sin añadir el tipo de dato utilizado
+let texto = 'Hola mundo! Esto es Typescript';
+console.log(`${texto}... texto añadido desde una variable!`);
+
+// [TIPADO FUERTE] → Añadiendo el tipo de dato
+let textoDos: string = 'Hola mundo! Esto es Typescript';
+console.log(`${textoDos}... añadiendo el tipo de dato!`);
+
+// textoDos = 2;
+// console.log(`${textoDos}... cambiandole a un tipo de dato erroneo!`);
